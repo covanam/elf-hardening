48c52b641b82ddd5eec961465c4af441c8d9ca6a
Nam Cao <namcaov@gmail.com>
2023-02-12 11:57:14 +0100

    simplify disassemble code

--- disasm.cpp
+++ disasm.cpp
@@ -11,8 +11,8 @@
 
 using namespace ELFIO;
 
-static std::vector<vins> code_to_bunit(const uint8_t *p, int size, uint64_t addr) {
-	std::vector<vins> ret;
+static std::list<vins> code_to_bunit(const uint8_t *p, int size, uint64_t addr) {
+	std::list<vins> ret;
 	csh handle;
 	cs_insn *insn;
 	size_t count;
@@ -36,97 +36,9 @@ static std::vector<vins> code_to_bunit(const uint8_t *p, int size, uint64_t addr
 	return ret;
 }
 
-struct raw_binary {
-	const uint8_t *data;
-	int size;
-	uint64_t addr;
-
-	raw_binary(const uint8_t *data, int size, uint64_t addr) :
-		data(data), size(size), addr(addr) {}
-};
-
-static std::tuple<std::list<raw_binary>, std::list<raw_binary>>
-split_text_section(const ELFIO::section *text, const symbol_section_accessor &sa) {
-	std::list<raw_binary> code, data;
-
-	raw_binary *last = nullptr;
-	raw_binary *curr = nullptr;
-
-	for (unsigned int i = 1; i < sa.get_symbols_num(); ++i ) {
-		std::string name;
-		Elf64_Addr value;
-		Elf_Xword size;
-		unsigned char bind;
-		unsigned char type;
-		Elf_Half section_index;
-		unsigned char other;
-
-		last = curr;
-
-		sa.get_symbol(i, name, value, size, bind,
-		type, section_index, other);
-
-		if (section_index != text->get_index())
-			continue;
-
-		const uint8_t *p = (const uint8_t*)text->get_data() + value;
-
-		/* gcc use $t and $d symbols to mark instructions and data */
-		if (name == "$d") {
-			data.push_back(raw_binary(p, 0, value));
-			curr = &data.back(); 
-		} else if (name == "$t") {
-			code.push_back(raw_binary(p, 0, value));
-			curr = &code.back();
-		} else {
-			continue;
-		}
-		
-		if (last != nullptr) {
-			last->size = curr->addr - last->addr;
-		}
-	}
-
-	curr->size = text->get_data() + text->get_size() - (char*)last->data; 
-
-	return std::tuple(code, data);
-}
-
-static std::list<vins> merge_instruction_data(
-	const std::list<std::vector<vins>> &inst,
-	const std::list<raw_binary> &data
-) {
+std::list<vins> disassemble(const ELFIO::elfio& reader) {
 	std::list<vins> ret;
 
-	auto d_itor = data.begin();
-	auto i_itor = inst.begin();
-
-	while (d_itor != data.end() || i_itor != inst.end()) {
-		if (i_itor == inst.end())
-			goto add_data;
-		if (d_itor == data.end())
-			goto add_inst;
-		if ((*i_itor)[0].addr < d_itor->addr)
-			goto add_inst;
-		goto add_data;
-
-		add_inst:
-		for (const vins &i : *i_itor) {
-			ret.push_back(i);
-		}
-		i_itor++;
-		continue;
-
-		add_data:
-		for (int i = 0; i < d_itor->size; ++i)
-			ret.push_back(vins(d_itor->data[i], d_itor->addr + i));
-		d_itor++;
-	}
-
-	return ret;
-}
-
-std::list<vins> disassemble(const ELFIO::elfio& reader) {
 	ELFIO::section *text = nullptr, *symtab;
 	for (int i = 0; i < reader.sections.size(); ++i) {
 		if (reader.sections[i]->get_name() == ".text") {
@@ -138,19 +50,53 @@ std::list<vins> disassemble(const ELFIO::elfio& reader) {
 
 	symbol_section_accessor symbols(reader, symtab);
 
-	auto temp = split_text_section(text, symbols);
-	std::list<raw_binary> code = std::get<0>(temp);
-	std::list<raw_binary> data = std::get<1>(temp);
+	struct region {
+		bool is_data;
+		Elf64_Addr offset;
+		Elf_Xword size;
+	};
+	std::list<region> regions;
 
-	std::list<std::vector<vins>> insn_list(code.size());
+	for (unsigned int i = 1; i < symbols.get_symbols_num(); ++i ) {
+		std::string name;
+		Elf64_Addr value;
+		Elf_Xword size;
+		unsigned char bind;
+		unsigned char type;
+		Elf_Half section_index;
+		unsigned char other;
 
-	auto il = insn_list.begin();
-	for (const raw_binary &b : code) {
-		*il = code_to_bunit(b.data, b.size, b.addr);
-		++il;
+		symbols.get_symbol(i, name, value, size, bind,
+		type, section_index, other);
+
+		if (section_index != text->get_index())
+			continue;
+
+		/* gcc use $t and $d symbols to mark instructions and data */
+		if (name == "$d") {
+			regions.back().size = value - regions.back().offset;
+			regions.push_back(region{true, value, 0});
+		} else if (name == "$t") {
+			regions.back().size = value - regions.back().offset;
+			regions.push_back(region{false, value, 0});
+		}
 	}
 
-	return merge_instruction_data(insn_list, data);
+	regions.back().size = text->get_size() - regions.back().offset; 
+
+	for (const region &r : regions) {
+		const uint8_t *data = (const uint8_t *)text->get_data() + r.offset;
+		if (r.is_data) {
+			for (unsigned i = 0; i < r.size; ++i) {
+				ret.push_back(vins(data[i], r.offset + i));
+			}
+		}
+		else {
+			ret.splice(ret.end(), code_to_bunit(data, r.size, r.offset));
+		}
+	}
+
+	return ret;
 }
 
 std::vector<uint8_t> assemble(const std::string &s) {
@@ -261,8 +207,8 @@ vins::vins(uint8_t data, uint64_t addr) {
 
 vins::vins(const std::string s, uint64_t addr) {
 	std::vector<uint8_t> bin = assemble(s);
-	static std::vector<vins> t = code_to_bunit(&bin[0], bin.size(), addr);
-	*this = t[0];
+	static std::list<vins> t = code_to_bunit(&bin[0], bin.size(), addr);
+	*this = t.front();
 }
 
 std::ostream& operator<<(std::ostream& os, const vins &b) {
@@ -373,6 +319,8 @@ void lifter::construct_labels() {
 			continue;
 		auto temp = std::find_if(instructions.begin(), instructions.end(),
 			[addr](vins &t) { return addr == t.addr; });
+		if (temp == this->instructions.end())
+			continue;
 	
 		if (temp->label.empty()) {
 			temp->label = ".L" + std::to_string(local_label_counter);
